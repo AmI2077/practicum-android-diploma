3d36e990fd6cf0f477ceca8c9817458b631c2fd0
Timon <Timon994@yandex.ru>
2026-07-10 20:11:08 +0300

    [#3] Подключил зависимости Room, Coroutines, Navigation, Koin, Lifecycle, Retrofit, Gson, Glide, Safeargs-plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.12.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
     id("convention.detekt")
+    alias(libs.plugins.kotlin.ksp) apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.7" apply false
 }
