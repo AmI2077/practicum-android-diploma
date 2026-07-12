0d4af70a1f8e251de7ce11772c5837e4d9343d23
Ami <d.dimaaaaaa@ya.ru>
2026-07-12 15:32:45 +0300

    Добавил панель навигации и fragmentContainer в activity_main.xml. Добавил шрифты для текста и styles.xml

--- app/src/main/java/ru/practicum/android/diploma/core/android/MainActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/android/MainActivity.kt
@@ -7,7 +7,7 @@ import ru.practicum.android.diploma.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        setContentView(R.layout.activity_main)
     }
 }
 
