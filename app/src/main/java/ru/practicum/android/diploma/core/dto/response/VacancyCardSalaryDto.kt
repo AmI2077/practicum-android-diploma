ed709aaa024a2317d5e560c745bfac39c26f0df0
Ami <d.dimaaaaaa@ya.ru>
2026-07-16 21:55:10 +0300

    Подправил типы данных в VacancyCardSalaryDto.kt

--- app/src/main/java/ru/practicum/android/diploma/core/dto/response/VacancyCardSalaryDto.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/dto/response/VacancyCardSalaryDto.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class VacancyCardSalaryDto(
     @SerializedName("from")
-    val from: String?,
+    val from: Int?,
     @SerializedName("to")
-    val to: String?,
+    val to: Int?,
     @SerializedName("currency")
     val currency: String?
 )
