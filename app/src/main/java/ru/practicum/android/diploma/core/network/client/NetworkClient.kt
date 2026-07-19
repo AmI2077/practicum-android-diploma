2393f894b4943ebde03a82142cfb1b2ef0c0987b
Ami <d.dimaaaaaa@ya.ru>
2026-07-19 19:36:50 +0300

    Обновил network клиент для загрузки вакансии по id

--- app/src/main/java/ru/practicum/android/diploma/core/network/client/NetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/network/client/NetworkClient.kt
@@ -3,8 +3,11 @@ package ru.practicum.android.diploma.core.network.client
 import ru.practicum.android.diploma.core.dto.request.VacancyRequestDto
 import ru.practicum.android.diploma.core.dto.response.VacancyResponseDto
 import ru.practicum.android.diploma.core.network.NetworkResult
+import ru.practicum.android.diploma.feature.detail.data.dto.VacancyDetailsDto
 
 interface NetworkClient {
 
     suspend fun fetchVacancies(vacancyRequestDto: VacancyRequestDto): NetworkResult<VacancyResponseDto?>
+
+    suspend fun fetchVacancyDetails(vacancyId: String): NetworkResult<VacancyDetailsDto?>
 }
