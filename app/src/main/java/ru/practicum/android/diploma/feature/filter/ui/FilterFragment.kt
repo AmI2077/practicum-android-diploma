9c8dcee672389f84ccf9466872455a57e851b68d
Timon <Timon994@yandex.ru>
2026-07-23 20:09:54 +0300

    [#60] Экран фильтра Настроена смена ic чекбокса Подсказка в поле ввода меняет цвет при фокусе

--- app/src/main/java/ru/practicum/android/diploma/feature/filter/ui/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/ui/FilterFragment.kt
@@ -1,9 +1,15 @@
 package ru.practicum.android.diploma.feature.search.ui
 
 import android.os.Bundle
+import android.util.TypedValue
+import ru.practicum.android.diploma.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.annotation.AttrRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
@@ -12,6 +18,8 @@ class FilterFragment : Fragment() {
 
     private var _binding: FragmentFilterBinding? = null
     private val binding get() = _binding!!
+
+    private var hideWithoutSalary = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,10 +36,62 @@ class FilterFragment : Fragment() {
         binding.backButton.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        binding.salaryEdit.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.salaryLabel.setTextColor(
+                    ContextCompat.getColor(requireContext(), R.color.blue)
+                )
+            } else {
+                binding.salaryLabel.setTextColorAttr(android.R.attr.textColorHint)
+            }
+        }
+
+        binding.hideWithoutSalaryContainer.setOnClickListener {
+            hideWithoutSalary = !hideWithoutSalary
+            updateSalaryCheckBox()
+        }
+
+        binding.mainContainer.setOnClickListener {
+            clearSalaryFocus()
+        }
+    }
+
+    fun TextView.setTextColorAttr(@AttrRes attrRes: Int) {
+        val typedValue = TypedValue()
+        context.theme.resolveAttribute(attrRes, typedValue, true)
+        setTextColor(typedValue.data)
+    }
+
+    private fun clearSalaryFocus() {
+        binding.salaryEdit.clearFocus()
+        hideKeyboard()
+    }
+
+    private fun hideKeyboard(){
+        val imm =
+            requireContext()
+                .getSystemService(
+                    InputMethodManager::class.java
+                )
+        imm?.hideSoftInputFromWindow(
+            binding.salaryEdit.windowToken,
+            0
+        )
+    }
+
+    private fun updateSalaryCheckBox() {
+        binding.salaryCheckBox.setImageResource(
+            if (hideWithoutSalary)
+                R.drawable.ic_check_box_on_24
+            else
+                R.drawable.ic_check_box_off_24
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
