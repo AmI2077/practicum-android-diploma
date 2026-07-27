a7cbca34bdf1d2ea8ee44d409540e8a2129b88f6
Timon <Timon994@yandex.ru>
2026-07-27 12:14:27 +0300

    [#] fix на предупреждения

--- app/src/main/java/ru/practicum/android/diploma/core/di/domainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/di/domainModule.kt
@@ -2,8 +2,6 @@ package ru.practicum.android.diploma.core.di
 
 import org.koin.dsl.module
 import ru.practicum.android.diploma.feature.detail.domain.usecase.GetVacancyDetailUseCase
-import ru.practicum.android.diploma.feature.favourites.data.repository.FavouritesRepositoryImpl
-import ru.practicum.android.diploma.feature.favourites.domain.api.FavouritesRepository
 import ru.practicum.android.diploma.feature.favourites.domain.interactor.FavouritesInteractor
 import ru.practicum.android.diploma.feature.favourites.domain.interactor.FavouritesInteractorImpl
 import ru.practicum.android.diploma.feature.filter.domain.interactor.FilterInteractor
--- app/src/main/java/ru/practicum/android/diploma/feature/filter/data/FilterRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/data/FilterRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.feature.filter.data
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import ru.practicum.android.diploma.core.models.filter.FilterIndustry
 import ru.practicum.android.diploma.feature.filter.domain.api.FilterRepository
 import ru.practicum.android.diploma.feature.filter.domain.models.FilterSettings
@@ -39,28 +40,25 @@ class FilterRepositoryImpl(
     }
 
     override fun saveFilterSettings(settings: FilterSettings) {
-        val editor = sharedPreferences.edit()
-
-        editor.putInt(KEY_SALARY, settings.salary ?: 0)
-        editor.putBoolean(KEY_HIDE_WITHOUT_SALARY, settings.hideWithoutSalary)
-
-        if (settings.industry != null) {
-            editor.putInt(KEY_INDUSTRY_ID, settings.industry.id)
-            editor.putString(KEY_INDUSTRY_NAME, settings.industry.name)
-        } else {
-            editor.remove(KEY_INDUSTRY_ID)
-            editor.remove(KEY_INDUSTRY_NAME)
+        sharedPreferences.edit {
+            putInt(KEY_SALARY, settings.salary ?: 0)
+            putBoolean(KEY_HIDE_WITHOUT_SALARY, settings.hideWithoutSalary)
+            if (settings.industry != null) {
+                putInt(KEY_INDUSTRY_ID, settings.industry.id)
+                putString(KEY_INDUSTRY_NAME, settings.industry.name)
+            } else {
+                remove(KEY_INDUSTRY_ID)
+                remove(KEY_INDUSTRY_NAME)
+            }
         }
-
-        editor.apply()
     }
 
     override fun clearFilterSettings() {
-        sharedPreferences.edit()
-            .remove(KEY_SALARY)
-            .remove(KEY_HIDE_WITHOUT_SALARY)
-            .remove(KEY_INDUSTRY_ID)
-            .remove(KEY_INDUSTRY_NAME)
-            .apply()
+        sharedPreferences.edit {
+            remove(KEY_SALARY)
+            remove(KEY_HIDE_WITHOUT_SALARY)
+            remove(KEY_INDUSTRY_ID)
+            remove(KEY_INDUSTRY_NAME)
+        }
     }
 }
